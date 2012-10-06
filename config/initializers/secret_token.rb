ab83564c3a7e7b59b201b5ae261fc24ad7321864
oliv <oliv@kernelle.localdomain>
2012-10-06 12:17:10 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Volocommunity::Application.config.secret_token = 'b133c79b871eecbaf4073518df1675f78b2c186b115cf823e25451aa3ffbb5ae03aed8c89e720a4f837683e5edbb41c46d780b804f9a12eccff6c86927219a5f'

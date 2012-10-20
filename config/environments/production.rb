5743086e4867f746dfa35d52bbf0f7124d7cef15
olivattaque <olivattaque@gmail.com>
2012-10-20 19:47:00 +0200

    fixing mail config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ Volocommunity::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
+  config.action_mailer.default_url_options = { :host => 'volo-community.herokuapp.com' }
+  
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
